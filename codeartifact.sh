53692a9fec01c335b617827aa7785e189aec5931
Jon Bidston <jbidston@gmail.com>
2021-08-20 13:49:18 +0100

    Added new shell script

---
+++ codeartifact.sh
@@ -0,0 +1,8 @@
+cat > ~/.aws/credentials <<EOL
+[default]
+region = $ENV_DEFAULT_REGION
+aws_secret_access_key = $ENV_SECRET_ACCESS_KEY
+aws_access_key_id = $ENV_ACCESS_KEY_ID
+EOL
+
+aws codeartifact login --tool npm --repository tvg-repository --domain tvg --domain-owner ${AWS_ACCOUNT_ID}
