f219ad0adb651b69b861f9e7dadcec95dcba94d0
Jon Bidston <jbidston@gmail.com>
2021-08-20 13:52:09 +0100

    Added credentials file location

--- codeartifact.sh
+++ codeartifact.sh
@@ -1,8 +1,10 @@
-cat > ~/.aws/credentials <<EOL
+cat > ./aws <<EOL
 [default]
 region = $ENV_DEFAULT_REGION
 aws_secret_access_key = $ENV_SECRET_ACCESS_KEY
 aws_access_key_id = $ENV_ACCESS_KEY_ID
 EOL
+
+export AWS_SHARED_CREDENTIALS_FILE=./aws
 
 aws codeartifact login --tool npm --repository tvg-repository --domain tvg --domain-owner ${AWS_ACCOUNT_ID}
